c46cd0a8642a4d625a3e068bdac225c28ab55a73
jeff.wade <jeff.s.wade@rtx.com>
2025-11-11 08:42:03 -0500

    Fix fcst_forcing_config function

--- src/mswm/build_inputs.py
+++ src/mswm/build_inputs.py
@@ -1174,8 +1174,8 @@ class RealizationBuilder:
 
             if self.forcing_configuration not in ['nwm', 'aorc']:
                 # Update dynamic parameters in forcing engine configuration file
-                gfun.update_forcing_config(self.cycle_date, self.cycle_hour, self.root_dir, self.forcing_template, gpkg_file, self.forcing_config_dir,
-                                           self.forcing_config_file, self.use_cold_start, self.use_int_ana, self.hind_cycle, self.cold_start_datetime)
+                gfun.update_fcst_forcing_config(self.cycle_date, self.cycle_hour, self.root_dir, self.forcing_template, gpkg_file, self.forcing_config_dir,
+                                                self.forcing_config_file, self.use_cold_start, self.use_int_ana, self.hind_cycle, self.cold_start_datetime)
             else:
                 # Update historical dynamic parameters in forcing engine configuration file
                 gfun.update_hist_forcing_config(self.time_period, self.root_dir, self.forcing_template, gpkg_file, self.forcing_config_dir, self.forcing_config_file, self.run_type, self.global_domain)
